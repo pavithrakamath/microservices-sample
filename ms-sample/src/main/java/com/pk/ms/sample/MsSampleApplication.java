85e18ae01b392fcba9e4d9341cbf32a3c51e4b0a
Pavithra Kamath <pavithrakamathgp@gmail.com>
2018-05-20 17:50:58 +0530

    initial project setup

---
+++ ms-sample/src/main/java/com/pk/ms/sample/MsSampleApplication.java
@@ -0,0 +1,12 @@
+package com.pk.ms.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsSampleApplication.class, args);
+	}
+}
