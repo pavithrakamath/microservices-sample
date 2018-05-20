926abccc8295d133dff0650669f1eb91f8ae536a
Pavithra Kamath <pavithrakamathgp@gmail.com>
2018-05-20 18:48:29 +0530

    changes to make the limits min and max as configurations from application.properties

--- limits-service/src/main/java/com/pk/ms/limits/controllers/LimitsConfigurationController.java
+++ limits-service/src/main/java/com/pk/ms/limits/controllers/LimitsConfigurationController.java
@@ -1,18 +1,23 @@
 package com.pk.ms.limits.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.pk.ms.limits.LimitsServiceConfiguration;
 import com.pk.ms.limits.beans.LimitConfiguration;
 
 @RestController
 @RequestMapping("/limits-service")
 public class LimitsConfigurationController {
 
+	@Autowired
+	LimitsServiceConfiguration myconf;
+	
 	@GetMapping("/limits")
 	public LimitConfiguration retreiveLimitsFromConfiguration() {
-		return new LimitConfiguration(100, 1000);
+		return new LimitConfiguration(myconf.getMinimum(), myconf.getMaximum());
 
 	}
 }
